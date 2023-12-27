e79bc4d9901d33d70d52628236d69b6e86a5bd49
last-brain-cell <naadkd@gmail.com>
2023-12-27 17:09:33 +0530

    ok

--- main.py
+++ main.py
@@ -3,29 +3,35 @@ from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
 from custom_agent import CustomAgent
 from langchain.llms.vertexai import VertexAI
+import time
 import os
 import dotenv
 
 dotenv.load_dotenv(".env")
 
+
 def agent_config():
     return CustomAgent(
-        database_url=os.getenv("DATABASE_URL"),
-        llm=VertexAI(
-            model='gemini-pro',
-            verbose=True
-        ),
-        verbose=False)
+            database_url=os.getenv("DATABASE_URL"),
+            llm=VertexAI(
+                model='gemini-pro',
+                verbose=True
+            ),
+            verbose=False
+        )
 
 
-ml_models = {}
+agents = {}
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    ml_models["answer_to_everything"] = agent_config()
+    start = time.perf_counter()
+    agents["gemini"] = agent_config()
+    end = time.perf_counter()
+    print(f"Application started in {round(end - start):.2f}s")
     yield
-    ml_models.clear()
+    agents.clear()
 
 
 app = FastAPI(lifespan=lifespan)
@@ -34,12 +40,15 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/predict")
 async def predict(prompt: str):
     try:
-        agent = ml_models["answer_to_everything"]
-        result = agent.run(prompt)
-        print(result)
+        result = agents["gemini"] = agent_config()
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/")
+async def home():
+    return {"message": "Custom SQL Agent Web Application"}
 
 
 if __name__ == "__main__":
